469a5e747128d3ca0283af7f687dc7afb7ad4c97
hhjjhhss <13887373330.163.com>
2024-09-27 23:09:24 +0800

    加了个插件，可以更简单的实现智能寻路，你可以看一眼决定用哪个

--- Script/Player.cs
+++ Script/Player.cs
@@ -1,6 +1,8 @@
+using Pathfinding;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Runtime.CompilerServices;
 using TMPro;
@@ -10,40 +12,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //每一个格子节点
-    public class Node
-    {
-        public int x;
-        public int y;
-        public int gCost;
-        public int hCost;
-        public bool Walkable;//用来表示是否可以走
-        public Node parent;
-        public Node(int _x, int _y, bool _Walkable)
-        {
-            x = _x;
-            y = _y;
-            Walkable = _Walkable;
-        }
-        public int fCost
-        { get { return gCost + hCost; } }
-    }
     public Vector3 MousePosition;
-    public Vector3 Target;
-    public Vector2 TurnPoint;
     public Vector2 PlayerDirection;
     public Ray2D DetectRay;
     public RaycastHit2D m_RaycastHit;
-    public bool OnMove = false;
-    public int CurrentPathIndex = 0;
     public float Speed = 5.0f;
-    private Node[,] grid;
-    private Vector2Int gridSize;
-    List<Vector2Int> path = new List<Vector2Int>();
+    private int CurIndex = 0;
+    private Seeker m_seeker;
+    private List<Vector3> m_pathPoint;
     private LayerMask ObstacleMask;//障碍层，之后再设置
-    private void Start()
+    private void Awake()
     {
-        InitGrid(new Vector2Int(20, 20));
+        m_seeker = GetComponent<Seeker>();
+
     }
     void Update()
     {
@@ -56,6 +37,7 @@ public class Player : MonoBehaviour
         {
             //把鼠标坐标转换为世界坐标
             MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            MousePosition.z = 0;
             //发射射线
             m_RaycastHit = Physics2D.Raycast(MousePosition, transform.forward, 1.5f);
             if (m_RaycastHit.collider != null)
@@ -64,151 +46,30 @@ public class Player : MonoBehaviour
             }
             else
             {
-                Target = new Vector3(MousePosition.x, MousePosition.y, MousePosition.z);
-                path = FindPath(transform.position, Target);
-                if (path != null && path.Count > 0)
-                {
-                    CurrentPathIndex = 0;
-                    Target = new Vector3(path[0].x, path[0].y, 0);
-                    OnMove = true;
-                }
+                CreatePath(MousePosition);
             }
         }
     }
     public void Move()
     {
-        if (OnMove)
+        if (m_pathPoint != null)
         {
-            Vector2.MoveTowards(transform.position, Target, 5.0f * Time.deltaTime);
-            Debug.Log(path.Count);
-            if (Vector3.Distance(transform.position, Target) < 0.05f)
+            if (Vector3.Distance(transform.position, m_pathPoint[CurIndex]) < 0.4f)
             {
-                // 到达当前路径点，更新目标路径点
-                CurrentPathIndex++;
-                if (CurrentPathIndex >= path.Count)
-                {
-                    OnMove = false;
-                }
-                else
-                {
-                    Target = new Vector3(path[CurrentPathIndex].x, path[CurrentPathIndex].y, 0f);
-                }
+                CurIndex++;
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, m_pathPoint[CurIndex], 5.0f * Time.deltaTime);
             }
         }
     }
-    public void InitGrid(Vector2Int size)
+    public void CreatePath(Vector3 target)
     {
-        gridSize = size;
-        CreateGrid();
-    }
-    //新建网格地图
-    public void CreateGrid()
-    {
-        grid = new Node[gridSize.x, gridSize.y];
-        for (int x = 0; x < gridSize.x; x++)
+        CurIndex = 0;
+        m_seeker.StartPath(transform.position, target, path =>
         {
-            for (int y = 0; y < gridSize.y; y++)
-            {
-                Vector3 worldPoint = new Vector3(x, y, 0);
-                bool walkable = !Physics2D.OverlapCircle(worldPoint, 0.1f, ObstacleMask);
-                grid[x, y] = new Node(x, y, walkable);
-            }
-        }
-    }
-    public List<Vector2Int> FindPath(Vector3 startPos, Vector3 targetPos)
-    {
-        Node targetNode =new Node(Mathf.RoundToInt(targetPos.x), Mathf.RoundToInt(targetPos.y), true);
-        Node startNode = new Node(Mathf.RoundToInt(startPos.x),Mathf.RoundToInt(startPos.y), true);
-        List<Node> openSet = new List<Node>();
-        HashSet<Node> closedSet = new HashSet<Node>();
-        openSet.Add(startNode);
-
-        while (openSet.Count > 0)
-        {
-            Node currentNode = openSet[0];
-            for (int i = 1; i < openSet.Count; i++)
-            {
-                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
-                {
-                    currentNode = openSet[i];
-                }
-            }
-
-            openSet.Remove(currentNode);
-            closedSet.Add(currentNode);
-
-            if (currentNode == targetNode)
-            {
-                return RetracePath(startNode, targetNode);
-            }
-
-            foreach (Node neighbor in GetNeighbors(currentNode))
-            {
-                if (!neighbor.Walkable || closedSet.Contains(neighbor))
-                {
-                    continue;
-                }
-
-                int newCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
-                if (newCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor))
-                {
-                    neighbor.gCost = newCostToNeighbor;
-                    neighbor.hCost = GetDistance(neighbor, targetNode);
-                    neighbor.parent = currentNode;
-
-                    if (!openSet.Contains(neighbor))
-                    {
-                        openSet.Add(neighbor);
-                    }
-                }
-            }
-        }
-
-        return null; // 如果找不到路径，返回空
-    }
-
-    private List<Vector2Int> RetracePath(Node startNode, Node endNode)
-    {
-        List<Vector2Int> path = new List<Vector2Int>();
-        Node currentNode = endNode;
-
-        while (currentNode != startNode)
-        {
-            path.Add(new Vector2Int(currentNode.x, currentNode.y));
-            currentNode = currentNode.parent;
-        }
-
-        path.Reverse();
-        return path;
-    }
-
-    private List<Node> GetNeighbors(Node node)
-    {
-        List<Node> neighbors = new List<Node>();
-        for (int x = -1; x <= 1; x++)
-        {
-            for (int y = -1; y <= 1; y++)
-            {
-                if (x == 0 && y == 0)
-                    continue;
-
-                int checkX = node.x + x;
-                int checkY = node.y + y;
-
-                if (checkX >= 0 && checkX < gridSize.x && checkY >= 0 && checkY < gridSize.y)
-                {
-                    neighbors.Add(grid[checkX, checkY]);
-                }
-            }
-        }
-
-        return neighbors;
-    }
-
-    private int GetDistance(Node nodeA, Node nodeB)
-    {
-        int distX = Mathf.Abs(nodeA.x - nodeB.x);
-        int distY = Mathf.Abs(nodeA.y - nodeB.y);
-        return distX + distY;
+            m_pathPoint = new List<Vector3>(path.vectorPath);
+        });
     }
 }
